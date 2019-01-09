409560836d7c7d5b5e10897b196dcd9b5e5cd35e
Arunkumar <mak@qxf2.com>
2019-01-09 13:56:27 +0530

    Changed the method name and continue game method

--- guess_number_game.py
+++ guess_number_game.py
@@ -15,10 +15,10 @@ class Player:
         self.score = 0
         self.guess = None 
     
-    def increment_score(self,points=1):
+    def increment_score(self,points):
         "Add points to the player's score"
         self.score += points
-
+       
     def get_score(self):
         "Return the score"
         return self.score    
@@ -29,14 +29,12 @@ class Player:
     
     def set_name(self,name):
         "Set the name of the player"
-        self.name = name
-        return self.name
+        self.name = name       
 
     def set_guess(self, number):
         "Set the guess of the player"
         self.guess = number
-        return self.guess
-
+        
     def get_guess(self):
         "Get the player's current guess"
         return self.guess
@@ -56,99 +54,96 @@ class Game:
 
     def start_game(self):
         "Start the game"
-        self.get_players_names()        
-        game_continue = True
-        while game_continue:
-            self.start_round()       
+        self.get_players_names()                     
+        self.start_play()        
+        self.continue_game()
+        self.determine_final_game_winner()
+        self.print_result()
 
     def print_rules(self):
         "Print the rules of the game"
-        pass
+        print('1.Each player can enter one number at a time')
+        print('2.Player 2 cannot repeat the same number of player 1')
+        print('3.Enter the number between 1 to 10 only')
 
     def get_players_names(self):
         "Get the player names from the user"
-        for i in range(1,self.number_of_players+1):            
-            self.player_name = raw_input("Hi Player enter your name")
-            self.player_object = Player(self.player_name)            
-            self.player_object.get_name()
-            self.players.append(self.player_name)
-            print self.players
-        #return self.players       
-
-    def start_round(self):
-        "Start a new round"
-        self.magic_number = self.generate_random_numbers()
+        for i in range(0,self.number_of_players):            
+            player_name = raw_input("Hi Player enter your name")
+            player_object = Player(player_name)           
+            self.players.append(player_object)
+            
+    def start_play(self):
+        "Start a new round"    
+        self.generate_random_numbers()
         self.collect_guesses()
-        #self.determine_winners()
-        self.update_player_scores()
-        self.continue_game()
+        self.determine_winners()
+        self.update_player_scores()               
         
     def generate_random_numbers(self):
         "Generate a random number"
-        self.magic_number = random.randint(1,10) 
-        print 'magic number is',self.magic_number       
+        self.magic_number = random.randint(1,10)               
         return self.magic_number
 
-    def determine_winners(self,player):
-        "Determine the winner(s)"        
-        #for player in self.players:
-        difference = abs(self.player_object.get_guess() - self.magic_number)
-        if difference == self.winning_difference:
-            self.winners.append(player)
-        if difference < self.winning_difference:            
-            self.winners = [player]
-        self.winning_difference = difference
-               
+    def determine_winners(self):
+        "Determine the winner(s)"
+        self.winners = []
+        winning_difference = 10000
+        for player in self.players:
+            difference = abs(player.get_guess() - self.magic_number)           
+            if difference == winning_difference:
+                self.winners.append(player)
+            if difference < winning_difference:
+                self.winners = [player]
+            winning_difference = difference        
+        
     def collect_guesses(self):
         "Collect the guesses"
         for player in self.players:
-            self.player_guess = int(raw_input("Hi, %s Enter your guess"%player))
-            self.player_object.set_guess(self.player_guess)
-            self.determine_winners(player)   
-        
+            player_guess = int(raw_input("Hi Player,Enter your guess"))
+            player.set_guess(player_guess)
+           
     def update_player_scores(self):
         "Update the winners scores"
-        for player in self.winners:
-            self.player_object.set_name(player)
-            self.player_object.increment_score(self.winning_points)
+        for player in self.winners:           
+            player.increment_score(self.winning_points)              
+            print('The winner of the round score is',player.get_score(),player.get_name())        
 
     def continue_game(self):
         "Ask players if they should continue the game"
-        print("Press 'Y' if you want to continue your game")
-        player_decision = raw_input()
-        if player_decision == 'Y':
-            self.start_round()
+        print("Press 1 to continue your game or press 2 to stop the game")
+        game_choice = input()
+        if game_choice == 1:
+            self.start_play()
+            self.continue_game()
         else:
-            self.print_result()       
-
-    def print_result(self):
-        "Print the result of the game"       
-        for player in self.winners:
-            print 'player',player
-            print self.player_object.set_name(player)
-            print self.player_object.get_score()
-            if self.player_object.get_score() == self.winning_score:
-                self.winners.append(player)
-            if self.player_object.get_score() > self.winning_score:
-                self.winners = [player]
-                print self.winners
-        #self.winning_score = self.player_object.get_score()
-        print self.winning_score
+            print("You have decided to stop the game,Now you can view your score board")        
         
-        print "The winner(s) list: "
-        for player in self.winners:
-            print self.player_object.get_name()
-            print "The winning score was:"
-            self.player_object.get_score()
-
-        """print "The scoreboard looked like this:"
+    def determine_final_game_winner(self):
+        "Determine the final winner of the game"        
+        self.winners = []
+        winning_score = -1
         for player in self.players:
-            print self.player_object.get_name(), self.player_object.get_score()"""
+            if player.get_score() == winning_score:
+                self.winners.append(player)
+            if player.get_score() > winning_score:
+                self.winners = [player]        
+        
+    def print_result(self):
+        "Print the result of the game"        
+        print("The winner(s) list: ")
+        for player in self.winners:
+            print player.get_name()
+        print("The winning score was:")
+        player.get_score()
+
+        print("The scoreboard looked like this:")
+        for player in self.players:
+            print player.get_name(), player.get_score()
         
 # Program starts here
 if __name__ == "__main__":
-    number_of_players = int(raw_input("Enter the number of players"))
-    #player = Player()
+    number_of_players = int(raw_input("Enter the number of players"))    
     game = Game(number_of_players)
     game.print_rules()
     game.start_game()
