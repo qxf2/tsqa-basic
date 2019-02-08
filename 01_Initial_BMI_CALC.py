849a33ea53692c54305eafb6acb1a87d140a9ac4
nelabhotlaR <47164371+nelabhotlaR@users.noreply.github.com>
2019-02-08 22:30:32 +0530

    added if and else condition

--- 01_Initial_BMI_CALC.py
+++ 01_Initial_BMI_CALC.py
@@ -2,4 +2,8 @@
 W = float(input('Enter Weight:'))
 H = float(input('Enter Height:'))
 BMI=round(W/H)
+if (BMI = 2) then
+print("Ower weight")
+else
+print("correct weight")
 print("BODY MASS INDIX",BMI)
