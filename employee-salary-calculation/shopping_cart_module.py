d0874b0de8284ec2c16cd6292e4cac4d84d497a0
Rahul.Bhave <rahul.bhave@qxf2.com>
2019-09-23 11:58:56 +0530

    exercise solved

--- employee-salary-calculation/shopping_cart_module.py
+++ employee-salary-calculation/shopping_cart_module.py
@@ -5,10 +5,10 @@ class ShoppingCart:
     
     def add_item(self,item):
         "Adding and updating the items into the cart"        
-        if not self.shopping_cart.has_key(item.item_name):            
+        if item.item_name not in self.shopping_cart.keys():          
             self.shopping_cart.update({item.item_name:item.item_quantity})
         else:
-            for key,value in self.shopping_cart.iteritems():
+            for key,value in self.shopping_cart.items():
                 if key == item.item_name and value == item.item_quantity:
                     print ("The item and quantity you are trying to add are in the shopping cart already,no need to add it")
                 
@@ -22,7 +22,7 @@ class ShoppingCart:
         "Removing an item from the list"        
         if not self.shopping_cart:
             print("There is no shopping cart and you cant delete it")
-        for key,values in self.shopping_cart.iteritems():               
+        for key,values in self.shopping_cart.items():               
             if key ==item.item_name and values == item.item_quantity:
                 item_to_remove = key
         self.shopping_cart.pop(item_to_remove)
